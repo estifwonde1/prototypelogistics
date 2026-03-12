af9790a03538e9ec640c6270f0d25eac0a35a138
z-mahlet <mahletaddisu123@gmail.com>
2026-03-12 18:03:18 +0300

    migrated models from cats core

---
+++ backend/warehouse-backend/engines/cats_warehouse/db/migrate/20260312190000_create_cats_warehouse_stores.rb
@@ -0,0 +1,23 @@
+class CreateCatsWarehouseStores < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cats_warehouse_stores do |t|
+      t.string :code
+      t.string :name, null: false
+      t.float :length, null: false
+      t.float :width, null: false
+      t.float :height, null: false
+      t.float :usable_space, null: false
+      t.float :available_space, null: false
+      t.boolean :temporary, default: false, null: false
+      t.boolean :has_gangway, default: false, null: false
+      t.float :gangway_length
+      t.float :gangway_width
+      t.float :gangway_corner_dist
+      t.references :warehouse,
+                   null: false,
+                   foreign_key: {to_table: :cats_warehouse_warehouses}
+
+      t.timestamps
+    end
+  end
+end
---
+++ backend/warehouse-backend/engines/cats_warehouse/db/migrate/20260312190030_create_cats_warehouse_inventory_adjustments.rb
@@ -0,0 +1,19 @@
+class CreateCatsWarehouseInventoryAdjustments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cats_warehouse_inventory_adjustments do |t|
+      t.string :reference_no
+      t.float :quantity
+      t.string :reason_for_adjustment
+      t.date :adjustment_date, null: false
+      t.string :status, default: "Draft", null: false
+      t.references :unit,
+                   null: false,
+                   foreign_key: {to_table: :cats_core_unit_of_measures}
+      t.references :stack,
+                   null: false,
+                   foreign_key: {to_table: :cats_warehouse_stacks}
+
+      t.timestamps
+    end
+  end
+end
