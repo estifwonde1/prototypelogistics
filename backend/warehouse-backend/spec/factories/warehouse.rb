f565f3601d3ab0501f1948514ed06ec5b38e84ff
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-04-03 22:39:08 +0300

    fix: add missing factories for warehouse models

--- backend/warehouse-backend/spec/factories/warehouse.rb
+++ backend/warehouse-backend/spec/factories/warehouse.rb
@@ -45,4 +45,43 @@ FactoryBot.define do
     warehouse { association :cats_warehouse_warehouse }
     role_name { "Warehouse Manager" }
   end
+
+  factory :cats_warehouse_stock_balance, class: "Cats::Warehouse::StockBalance" do
+    warehouse { association :cats_warehouse_warehouse }
+    store { association :cats_warehouse_store }
+    stack { association :cats_warehouse_stack }
+    commodity { association :cats_core_commodity }
+    unit { association :cats_core_unit_of_measure }
+    quantity { 100 }
+  end
+
+  factory :cats_warehouse_grn, class: "Cats::Warehouse::Grn" do
+    warehouse { association :cats_warehouse_warehouse }
+    received_by { association :cats_core_user }
+    received_on { Date.today }
+    reference_no { "GRN-#{generate(:core_code)}" }
+  end
+
+  factory :cats_warehouse_gin, class: "Cats::Warehouse::Gin" do
+    warehouse { association :cats_warehouse_warehouse }
+    issued_by { association :cats_core_user }
+    issued_on { Date.today }
+    reference_no { "GIN-#{generate(:core_code)}" }
+  end
+
+  factory :cats_warehouse_inspection, class: "Cats::Warehouse::Inspection" do
+    warehouse { association :cats_warehouse_warehouse }
+    inspector { association :cats_core_user }
+    inspected_on { Date.today }
+    reference_no { "INSP-#{generate(:core_code)}" }
+    source_type { "Grn" }
+    source_id { 1 }
+  end
+
+  factory :cats_warehouse_waybill, class: "Cats::Warehouse::Waybill" do
+    warehouse { association :cats_warehouse_warehouse }
+    issued_by { association :cats_core_user }
+    issued_on { Date.today }
+    reference_no { "WAY-#{generate(:core_code)}" }
+  end
 end
